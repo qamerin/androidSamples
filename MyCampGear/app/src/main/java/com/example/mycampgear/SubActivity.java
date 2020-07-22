4446de4e16c7ac07aed4b2c75154bb772bf496aa
hiroki kameyama <hkame@jp.ibm.com>
2020-07-23 07:05:40 +0900

    modify SubScreen layout

--- MyCampGear/app/src/main/java/com/example/mycampgear/SubActivity.java
+++ MyCampGear/app/src/main/java/com/example/mycampgear/SubActivity.java
@@ -3,10 +3,34 @@ package com.example.mycampgear;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
+    private static final String[] scenes = {
+            "Ventnor",
+            "Wroxall",
+            "Whitewell",
+            "Ryde",
+            "StLawrence",
+            "Lake",
+            "Sandown",
+            "Shanklin"
+    };
+
+    // ちょっと冗長的ですが分かり易くするために
+    private static final int[] photos = {
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +46,19 @@ public class SubActivity extends AppCompatActivity {
         textView.setText(selectedText);
         ImageView  imageView = findViewById(R.id.selected_photo);
         imageView.setImageResource(selectedPhoto);
+
+
+        // ListViewのインスタンスを生成
+        ListView listView = findViewById(R.id.list_view);
+
+        // BaseAdapter を継承したadapterのインスタンスを生成
+        // レイアウトファイル list.xml を activity_main.xml に
+        // inflate するためにadapterに引数として渡す
+        BaseAdapter adapter = new ListViewAdapter(this.getApplicationContext(),
+                R.layout.list, scenes, photos);
+
+        // ListViewにadapterをセット
+        listView.setAdapter(adapter);
+
     }
 }
