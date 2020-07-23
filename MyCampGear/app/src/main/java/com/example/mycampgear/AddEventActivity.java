925f40ed37beabaa40abdf8cd4122761770b8be4
hiroki kameyama <hkame@jp.ibm.com>
2020-07-24 08:18:09 +0900

    add event register screen

--- MyCampGear/app/src/main/java/com/example/mycampgear/AddEventActivity.java
+++ MyCampGear/app/src/main/java/com/example/mycampgear/AddEventActivity.java
@@ -14,15 +14,6 @@ public class AddEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
-        Intent intent = getIntent();
-        // MainActivityからintentで受け取ったものを取り出す
-        String selectedText = intent.getStringExtra("Text");
-        int selectedPhoto = intent.getIntExtra("Photo", 0);
-
-        TextView textView = findViewById(R.id.selected_text);
-        textView.setText(selectedText);
-        ImageView  imageView = findViewById(R.id.selected_photo);
-        imageView.setImageResource(selectedPhoto);
 
 
 
