939254bed5d473e8820ac2c4c04ee1d312d30554
hiroki kameyama <hkame@jp.ibm.com>
2020-07-25 07:05:14 +0900

    add item ddl

--- MyCampGear/app/src/main/java/com/example/mycampgear/db/EventOpenHelper.java
+++ MyCampGear/app/src/main/java/com/example/mycampgear/db/EventOpenHelper.java
@@ -10,7 +10,19 @@ public class EventOpenHelper extends SQLiteOpenHelper {
     private static final String FILE_NAME = "Event";
     private static final int VERSION = 1;
 
-    private static final String DDL = "CREATE TABLE Event ( _id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date DATE, content TEXT)";
+    private static final String DDL_EVENT = "CREATE TABLE Event (" +
+            " _event_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            " title TEXT," +
+            " date DATE," +
+            " content TEXT" +
+            ")";
+
+    private static final String DDL_ITEM = "CREATE TABLE ITEM ( " +
+            "_item_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            " event_id TEXT," +
+            " title TEXT," +
+            " date DATE, " +
+            "content TEXT)";
 
     public EventOpenHelper(Context context) {
         super(context, FILE_NAME, null, VERSION);
@@ -18,7 +30,8 @@ public class EventOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(DDL);
+        sqLiteDatabase.execSQL(DDL_EVENT);
+        sqLiteDatabase.execSQL(DDL_ITEM);
     }
 
     @Override
